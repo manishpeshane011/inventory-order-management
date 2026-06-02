d5626b4f78c5524d89188798110d385167aec202
manishpeshane011 <manishpeshane011@gmail.com>
2026-06-02 20:25:25 +0530

    Fix PostgreSQL config + Alembic setup for Render

--- PythonBackend/app/database.py
+++ PythonBackend/app/database.py
@@ -1,11 +1,11 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
-from dotenv import load_dotenv
 import os
 
-load_dotenv()
-
 DATABASE_URL = os.getenv("DATABASE_URL")
+
+if not DATABASE_URL:
+    raise Exception("DATABASE_URL not found in environment variables")
 
 engine = create_engine(
     DATABASE_URL,
